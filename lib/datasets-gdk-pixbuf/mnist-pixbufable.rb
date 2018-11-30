2082433dc66075f1542a021e419c5d43149e1fcd
Kouhei Sutou <kou@clear-code.com>
2018-12-01 06:14:36 +0900

    Fix wrong pack pattern

--- lib/datasets-gdk-pixbuf/mnist-pixbufable.rb
+++ lib/datasets-gdk-pixbuf/mnist-pixbufable.rb
@@ -4,7 +4,7 @@ module DatasetsGdkPixbuf
   module MNISTPixbufable
     def to_gdk_pixbuf
       rgb_data = pixels.map{|p| [(p - 255).abs] * 3}.flatten
-      GdkPixbuf::Pixbuf.new(data: rgb_data.pack("C"),
+      GdkPixbuf::Pixbuf.new(data: rgb_data.pack("C*"),
                             width: 28,
                             height: 28)
     end
